c74b452f576cd98656a2713e766d528a1a0c0c4b
Jennifer Haddock <jentx123@hotmail.com>
2017-09-06 01:38:20 +0000

    helper methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,12 @@
 class Team < ActiveRecord::Base
     belongs_to :user
     has_many :players
+
+    def slug
+      username.downcase.gsub(' ', '-')
+    end
+
+    def self.find_by_slug(slug)
+      self.all.detect{|a| a.username.downcase.gsub(' ', '-') == slug}
+    end
 end
