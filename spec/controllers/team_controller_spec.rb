7f68c2c45aa1e90b3b399ec79c7d876ea7bf7c66
Jennifer Haddock <jentx123@hotmail.com>
2017-09-09 04:33:30 +0000

    team controller edit specs

--- spec/controllers/team_controller_spec.rb
+++ spec/controllers/team_controller_spec.rb
@@ -96,4 +96,62 @@ require 'spec_helper'
         end
       end
 
+    describe 'edit action' do
+      context "logged in" do
+        it 'lets a user view team edit form if they are logged in' do
+          user = User.create(:name => "FantasyWinner", :email => "GoTeam@aol.com", :password => "SuperBowl")
+          team = Team.create(:name => "Best Team", :user_id => user.id)
+          visit '/login'
+
+          fill_in(:name, :with => "FantasyWinner")
+          fill_in(:password, :with => "SuperBowl")
+          click_button 'submit'
+          visit '/teams/best-team/edit'
+          expect(page.status_code).to eq(200)
+          expect(page.body).to include(team.name)
+        end
+
+        it 'does not let a user edit a team they did not create' do
+          user1 = User.create(:name => "FantasyWinner", :email => "GoTeam@aol.com", :password => "SuperBowl")
+          team1 = Team.create(:name => "Best Team", :user_id => user1.id)
+
+          user2 = User.create(:name => "I Love Football", :email => "FootballIsLife@aol.com", :password => "SuperBowl")
+          team2 = Team.create(:name => "New Team", :user_id => user2.id)
+
+          visit '/login'
+
+          fill_in(:name, :with => "FantasyWinner")
+          fill_in(:password, :with => "SuperBowl")
+          click_button 'submit'
+          session = {}
+          session[:user_id] = user1.id
+          visit "/teams/new-team/edit"
+          expect(page.current_path).to include('/edit')
+        end
+
+        it 'lets a user edit their own team if they are logged in' do
+          user = User.create(:name => "FantasyWinner", :email => "GoTeam@aol.com", :password => "SuperBowl")
+          team = Team.create(:name => "Best Team", :user_id => 1)
+          visit '/login'
+
+          fill_in(:name, :with => "FantasyWinner")
+          fill_in(:password, :with => "SuperBowl")
+          click_button 'submit'
+          visit '/teams/best-team/edit'
+
+          player1 = Player.create(:name => "Awesome RB", :position => "RB", :pro_team => "Hurricanes", :team_id => 1)
+
+          click_button 'Save'
+          expect(Team.find_by(:name => "Best Team")).to be_instance_of(Team)
+          expect(Team.find_by(:name => "Best Team").players).to include(player1)
+        end
+      end
+
+      context "logged out" do
+        it 'does not load let user view team edit form if not logged in' do
+          get '/teams/new-team/edit'
+          expect(last_response.location).to include("/login")
+        end
+      end
+    end
 end
