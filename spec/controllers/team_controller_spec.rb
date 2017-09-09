ae9bdb4ebf22083d64a1d4ba542a2a9d3953356c
Jennifer Haddock <jentx123@hotmail.com>
2017-09-09 03:37:06 +0000

    team controller show spec

--- spec/controllers/team_controller_spec.rb
+++ spec/controllers/team_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
-describe TeamController do
+  describe TeamController do
+    describe 'team show page' do
+      it 'shows all a single teams players' do
+        user = User.create(:name => "FantasyWinner", :email => "GoTeam@aol.com", :password => "SuperBowl")
+        team = Team.create(:name => "First Team", :user_id => user.id)
+        player1 = Player.create(:name => "Awesome RB", :position => "RB", :pro_team => "Hurricanes", :team_id => team.id)
+        player2 = Player.create(:name => "Okay QB", :position => "QB", :pro_team => "Dolphins", :team_id => team.id)
+        player3 = Player.create(:name => "Horrible DE", :position => "DE", :pro_team => "Detroit", :team_id => team.id)
+        params = {
+          :name => "FantasyWinner",
+          :password => "SuperBowl"
+        }
+        post '/login', params
 
+        get "/teams/#{team.slug}"
+
+        expect(last_response.body).to include("Awesome RB")
+        expect(last_response.body).to include("Okay QB")
+        expect(last_response.body).to include("Horrible DE")
+      end
+    end
 end
