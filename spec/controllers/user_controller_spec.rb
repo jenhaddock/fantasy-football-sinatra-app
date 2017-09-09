780ef608e6ffe029599481b6d357165ae79dfa37
Jennifer Haddock <jentx123@hotmail.com>
2017-09-09 02:28:14 +0000

    User Controller Signup Specs

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
 
+  describe "Signup Page" do
+    it 'loads the signup page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'signup directs user to index' do
+      params = {
+        :username => "FantasyWinner",
+        :email => "GoTeam@aol.com",
+        :password => "SuperBowl"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/index")
+    end
+
+
+
+  end
 end
